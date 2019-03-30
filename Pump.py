a585a84481a1fa7918e6036f197e60b384482a62
Adiel Hernandez <adielhernandez15@gmail.com>
2019-03-30 18:35:52 -0400

    Worked on the documentation of the Pump module.

--- Pump.py
+++ Pump.py
@@ -15,8 +15,29 @@ myport.connect()
 
 """
 class ThreePump():
+    """
+    This class is used for control of the Pump. A serial connection is established
+    with the microcontroller controlling the Pump.
+
+    Attributes:
+        baud (int): The baud rate the serial connection is using.
+        port (string): the microcontrollers port for the serial connection.
+        ser (serial Object): Instance of serial object representing the serial connection.
+        serialConnected (bool): True/False of whether the serial connection was established.
+        calibrationvolume (int): amount of fluid used for calibration.
+        calibrationunit (string): the unit for the amount of fluid used for calibration.
+        isOn (bool): True/False on whether the pump is on or not.
+
+    """
 
     def __init__(self,my_port):
+        """
+        The constructor for the TwoSwitch class.
+
+        Parameters:
+            my_port (string): the microcontrollers port for the serial connection.
+        """
+
         self.baud = 9600
         self.port = my_port
         self.ser = None
@@ -30,11 +51,16 @@ class ThreePump():
         self.setDefaults()
 
         self.isOn = None
-    def serial_connect(self):
-        """Once the comport of the pump is known we open a serial connection
-        to it using pySerial"""
 
-        print("This pump's port is: " + str(self.port) + " !!!!!!!!")
+    def serial_connect(self):
+        """
+        This method establishes the serial connection with the microcontroller.
+
+        Once the comport of the pump is known we open a serial connection
+        to it using pySerial.
+        """
+
+        #print("This pump's port is: " + str(self.port) + " !!!!!!!!")
 
         if self.port == None:
             self.connected = False
@@ -44,19 +70,25 @@ class ThreePump():
                 self.connected = True
             else:
                 self.connected = False
+
     def send(self,cmd):
         """
-        uses serial connection opened instance of pump and sends
-        the text written in cmd across that connection.
-        Function had option
+        This method sends a command across the serial connection.
+
+        Parameters:
+            cmd (string): The command or string that is to be sent to the microcontroller.
         """
+
         self.ser.write(cmd.encode('ascii') + '\r'.encode('ascii'))
         print("Sent a serial command: %s %s "%("Pump",cmd))
+
     def send_return(self,cmd):
         """
-        uses serial connection opened instance of pump and sends
+        This method uses the serial connection opened instance of pump and sends
         the text written in cmd across that connection.
-        Function had option
+
+        Returns:
+            response (string): The response or what the microcontroller returns.
         """
         self.ser.flushInput()
         self.ser.flushOutput()
@@ -70,14 +102,27 @@ class ThreePump():
         print(response)
 
         return response
+
     def chop_return(self,ret):
-        """output of the pump comes with a carriage return and newline at the end of itself.
-        We chop them off here.
+        """
+        This method modifies the output of the two switch which comes with a carriage return and newline at the end of itself.
+        The carriage return and newline is cut off here.
+
+        Returns:
+            Output of the pump without the carraige return and newline at the end.
         """
         if ret.endswith('\r\n'):
             ret = ret[:-2]
         return ret
+
     def FormatVolume(self,V,unit):
+        """
+        This method formats the volume.
+
+        Parameters:
+            V (int): The amount of volume.
+            unit (string): The unit the volume should be set in.
+        """
             if unit == "mL":
                 S = 1
             elif unit == "uL":
@@ -88,11 +133,27 @@ class ThreePump():
             D = str(int(float(A[:5])*1000))
             E = D+C
             return E
+
     def setFlow(self,channel,flowrate):
+        """
+        This method sets the flow rate of the pump.
+
+        Parameters:
+            channel (int): The channel whose flow rate will be set.
+            flowrate (int): The flow rate which is to be set.
+        """
         flowrate = self.FormatVolume(flowrate,"uL")
         print(flowrate)
         self.send(str(channel)+"f"+flowrate)
+
     def setDir(self,channel,direction):
+        """
+        This method sets the direction for the flow of fluid.
+
+        Parameters:
+            channel (int): The channel flow rate direction will be set.
+            direction (string): The direction for the pump CW or CCW.
+        """
 
         if direction == "CW":
             self.send(str(channel)+"J")
@@ -102,33 +163,91 @@ class ThreePump():
             self.send(str(channel) + "K")
             self.send(str(channel) + "xRK")
 
-
-
-
     def start(self,channel):
+        """
+        This method sends a command to the microcontroller which is programmed to
+        start the pump only for the channel chosen.
+
+        Parameters:
+            channel (int): the channel that will start.
+        """
         self.send(str(channel)+"H")
+
     def stop(self,channel):
+        """
+        This method sends a command to the microcontroller which is programmed to
+        stop the pump only for the channel chosen.
+
+        Parameters:
+            channel (int): the channel that will stop.
+        """
+
         self.send(str(channel)+"I")
+
     def calibrate(self,channel):
+        """
+        This method sends a command to the microcontroller which is programmed to
+        calibrate the pump only for the channel chosen.
+
+        Parameters:
+            channel (int): the channel that will be calibrated.
+        """
+
         self.send(str(channel)+"xY")
+
     def abort_calibration(self,channel):
+        """
+        This method sends a command to the microcontroller which is programmed to
+        abort calibration of the pump only for the channel chosen.
+
+        Parameters:
+            channel (int): the channel that will abort calibration.
+        """
+
         self.send(str(channel)+"xZ")
+
     def setTargetCalibrationVolume(self):
+        """
+        This method sets the target calibration volume.
+        """
+
         for i in range(3):
             to_send = str(i+1)+"x"+self.FormatVolume(self.calibrationvolume,self.calibrationunit)
             self.send(to_send)
+
     def setMeasuredVolume(self,channel,volume):
+        """
+        This method sets the measured volume.
+        """
+
         volume = self.FormatVolume(volume,"mL")
         self.send_return(str(channel)+"xV"+volume)
+
     def start_all(self):
+        """
+        This method sends a command to the microcontroller which is programmed to
+        start the pump for all channels.
+        """
+
         for i in range(3):
             self.send(str(i+1)+"H")
         self.isOn = True
+
     def stop_all(self):
+        """
+        This method sends a command to the microcontroller which is programmed to
+        stop the pump for all the channels.
+        """
+
         for i in range(3):
             self.send(str(i+1)+"I")
         self.isOn = False
+
     def setDefaults(self):
+        """
+        This method sets the deafults for the pump.
+        """
+
         #setting to flowrate mode
         for channel in range(3):
             self.send(str(channel+1)+"xU1000+0")
@@ -137,7 +256,7 @@ class ThreePump():
 
 def get_ports():
     """
-    Uses serial module's comports command to get and return serial ports
+    This method uses serial module's comports command to get and return serial ports.
     """
     ports = []
     for n, (port, desc, hwid) in enumerate(sorted(comports()), 1):
